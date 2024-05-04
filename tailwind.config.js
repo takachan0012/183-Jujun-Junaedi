52154faeae7ee2a5e449db95fc248f1230a292d1
takachan0012 <jj4068851@gmail.com>
2024-05-04 17:18:57 +0700

    feat: add daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui"
+
 export default {
   content: [
     "./resources/**/*.blade.php",
@@ -8,5 +10,7 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    daisyui,
+  ],
 }
