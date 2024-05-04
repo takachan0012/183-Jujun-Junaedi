ac2d24d4f5da9bb69695dacf5a0df2ebb3eb2a2d
takachan0012 <jj4068851@gmail.com>
2024-05-05 06:37:56 +0700

    feat: responsive sm

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
     <x-slot:title>{{ $title }}</x-slot:title>
     <div class="w-screen h-screen flex justify-center items-center">
-        <div class="w-96">
+        <div class="w-80 lg:w-96">
             <div class="flex items-center my-9 flex-col justify-center">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                     stroke="currentColor" class="w-6 h-6">
