440321eeecf8eec646ce0d510f62b204cd7830b9
takachan0012 <jj4068851@gmail.com>
2024-05-04 20:28:20 +0700

    feat: login ui

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -11,15 +11,19 @@ class FormController extends Controller
         // Validate the request data
         $validatedData = $request->validate([
             'username' => 'required|string|max:255',
+            'email' => 'required|email|max:255',
+            'password' => 'required|string|min:8',
         ]);
 
         // Retrieve the username from the request
         $username = $request->input('username');
+        $email = $request->input('email');
+        $password = $request->input('password');
 
         // Process the submitted data
         // You can save it to the database, perform additional actions, etc.
 
         // Return a response
-        return "Form submitted successfully. Username: $username";
+        return "Form submitted successfully. Username: $username, Email: $email, password: $password ";
     }
 }
