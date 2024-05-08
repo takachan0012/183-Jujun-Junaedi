5c6b4dcde696ed480193040147172b44dca6f5a5
takachan0012 <jj4068851@gmail.com>
2024-05-09 00:23:21 +0700

    fix: fixing profile

--- resources/views/components/layout-dashboard.blade.php
+++ resources/views/components/layout-dashboard.blade.php
@@ -13,6 +13,29 @@
                         kunciKas
                     </h1>
                 </div>
+                <div class="w-full flex justify-center">
+                    <ul class="menu lg:menu-horizontal bg-base-200 p-0">
+                        <li>
+                            <details>
+                                <summary class="flex items-center">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                        stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                                    </svg>
+                                    <div class="flex flex-col">
+                                        <span class="font-medium">Jujun</span>
+                                        <span class="font-light">role</span>
+                                    </div>
+                                </summary>
+                                <ul>
+                                    <li><a>Profile</a></li>
+                                    <li><a>Logout</a></li>
+                                </ul>
+                            </details>
+                        </li>
+                    </ul>
+                </div>
                 <div class="flex flex-col gap-3">
                     <a href="/dashboard">
                         <x-button
