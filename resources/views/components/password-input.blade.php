a1f2dc81ebfa451510307725880cff8b1ce0891d
takachan0012 <jj4068851@gmail.com>
2024-05-18 16:05:42 +0700

    feat: add props placeholder

--- resources/views/components/password-input.blade.php
+++ resources/views/components/password-input.blade.php
@@ -4,7 +4,8 @@
             d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
             clip-rule="evenodd" />
     </svg>
-    <input type="password" class="grow" placeholder="Password" name="password" id="password" required />
+    <input type="password" class="grow" placeholder="{{ $placeholder ?? 'Password' }}" name="password" id="password"
+        required />
     <div onclick="togglePasswordVisibility()" class="hover:cursor-pointer">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
             stroke="currentColor" class="w-6 h-6 opened-eye hidden">
