41b72f81620f11d4c8383af5258cd687eb870fb0
takachan0012 <jj4068851@gmail.com>
2024-05-19 22:18:59 +0700

    feat: change word

--- resources/views/users/dashboard.blade.php
+++ resources/views/users/dashboard.blade.php
@@ -1,7 +1,7 @@
 <x-layout-dashboard>
     <x-slot:content>{{ $content }}</x-slot:content>
     <div class="z-10 fixed top-0 w-full p-4 bg-base-100 h-16">
-        <h2 class="text-2xl">Welcome, Jujun Junaedi</h2>
+        <h2 class="text-2xl">Hello, Jujun Junaedi</h2>
     </div>
     <section class="my-16 flex gap-2">
         <a href="{{ route('debt') }}" class="h-48 w-1/2">
@@ -13,7 +13,7 @@
                         d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25" />
                 </svg>
                 <span>
-                    Debt
+                    Create Debt
                 </span>
             </div>
         </a>
@@ -26,7 +26,7 @@
                         d="M19.5 12c0-1.232-.046-2.453-.138-3.662a4.006 4.006 0 0 0-3.7-3.7 48.678 48.678 0 0 0-7.324 0 4.006 4.006 0 0 0-3.7 3.7c-.017.22-.032.441-.046.662M19.5 12l3-3m-3 3-3-3m-12 3c0 1.232.046 2.453.138 3.662a4.006 4.006 0 0 0 3.7 3.7 48.656 48.656 0 0 0 7.324 0 4.006 4.006 0 0 0 3.7-3.7c.017-.22.032-.441.046-.662M4.5 12l3 3m-3-3-3 3" />
                 </svg>
                 <span>
-                    Transaction
+                    Create Transaction
                 </span>
             </div>
         </a>
