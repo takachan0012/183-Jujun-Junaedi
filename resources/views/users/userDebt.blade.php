38755d0ea3ab92fe7b2556d3847ff33e8d782368
takachan0012 <jj4068851@gmail.com>
2024-05-20 22:06:38 +0700

    feat: add userDebt

--- resources/views/users/userDebt.blade.php
+++ resources/views/users/userDebt.blade.php
@@ -1,10 +1,10 @@
 <x-layout-dashboard>
     <x-slot:content>{{ $content }}</x-slot:content>
     <div class="flex justify-center">
-        <div class="w-1/2">
+        <div class="w-[80%]">
             <div class="flex flex-col gap-4">
                 <div class="flex flex-col sticky top-4 bg-base-100 z-10">
-                    <x-button-back />
+                    <x-button-back class="w-28" />
                     <span class="pt-7">Total Jujun Debt</span>
                     <div class="flex justify-between">
                         <span class="text-green-primary text-xl">Rp10,000</span>
@@ -18,49 +18,87 @@
                             <th>Date</th>
                             <th>Received</th>
                             <th>Given</th>
+                            <th>Category</th>
+                            <th>Note</th>
                         </tr>
                     </thead>
                     <tbody>
                         <!-- row 1 -->
                         <tr>
                             <td>
-                                <div class="flex items-center gap-3">
-                                    <div>
-                                        <span class="font-bold">18 Mei 2023</span>
-                                        <br />
-                                        <span class="text-xs">Note Here</span>
-                                    </div>
-                                </div>
+                                <span class="font-bold">18 Mei 2023</span>
                             </td>
                             <td>
                                 <span class="text-green-primary">Rp10,000</span>
+                            </td>
+                            <td>
+                                <span class="text-red-500"></span>
+                            </td>
+                            <td>
+                                <span>Note Here</span>
+                            </td>
+                            <td>
+                                <span>Category Here</span>
                             </td>
                         </tr>
                         <!-- row 2 -->
                         <tr>
                             <td>
-                                <div class="flex items-center gap-3">
-                                    <div>
-                                        <span class="font-bold">17 Mei 2023</span>
-                                        <br />
-                                        <span class="text-xs">Note Here</span>
-                                    </div>
-                                </div>
+                                <span class="font-bold">17 Mei 2023</span>
                             </td>
                             <td>
                                 <span class="text-green-primary">Rp10,000</span>
                             </td>
+                            <td>
+                                <span class="text-red-500"></span>
+                            </td>
+                            <td>
+                                <span>Note Here</span>
+                            </td>
+                            <td>
+                                <span>Category Here</span>
+                            </td>
                         </tr>
-                        <!-- row 3 -->
+                        <!-- row 1 -->
                         <tr>
                             <td>
-                                <div class="flex items-center gap-3">
-                                    <div>
-                                        <span class="font-bold">18 Mei 2023</span>
-                                        <br />
-                                        <span class="text-xs">Note Here</span>
-                                    </div>
-                                </div>
+                                <span class="font-bold">18 Mei 2023</span>
+                            </td>
+                            <td>
+                                <span class="text-green-primary">Rp10,000</span>
+                            </td>
+                            <td>
+                                <span class="text-red-500"></span>
+                            </td>
+                            <td>
+                                <span>Note Here</span>
+                            </td>
+                            <td>
+                                <span>Category Here</span>
+                            </td>
+                        </tr>
+                        <!-- row 2 -->
+                        <tr>
+                            <td>
+                                <span class="font-bold">17 Mei 2023</span>
+                            </td>
+                            <td>
+                                <span class="text-green-primary">Rp10,000</span>
+                            </td>
+                            <td>
+                                <span class="text-red-500"></span>
+                            </td>
+                            <td>
+                                <span>Note Here</span>
+                            </td>
+                            <td>
+                                <span>Category Here</span>
+                            </td>
+                        </tr>
+                        <!-- row 2 -->
+                        <tr>
+                            <td>
+                                <span class="font-bold">16 Mei 2023</span>
                             </td>
                             <td>
                                 <span class="text-green-primary"></span>
@@ -68,17 +106,53 @@
                             <td>
                                 <span class="text-red-500">Rp10,000</span>
                             </td>
+                            <td>
+                                <span>Note Here</span>
+                            </td>
+                            <td>
+                                <span>Category Here</span>
+                            </td>
                         </tr>
-                        <!-- row 3 -->
+                        <!-- row 1 -->
                         <tr>
                             <td>
-                                <div class="flex items-center gap-3">
-                                    <div>
-                                        <span class="font-bold">18 Mei 2023</span>
-                                        <br />
-                                        <span class="text-xs">Note Here</span>
-                                    </div>
-                                </div>
+                                <span class="font-bold">18 Mei 2023</span>
+                            </td>
+                            <td>
+                                <span class="text-green-primary">Rp10,000</span>
+                            </td>
+                            <td>
+                                <span class="text-red-500"></span>
+                            </td>
+                            <td>
+                                <span>Note Here</span>
+                            </td>
+                            <td>
+                                <span>Category Here</span>
+                            </td>
+                        </tr>
+                        <!-- row 2 -->
+                        <tr>
+                            <td>
+                                <span class="font-bold">17 Mei 2023</span>
+                            </td>
+                            <td>
+                                <span class="text-green-primary">Rp10,000</span>
+                            </td>
+                            <td>
+                                <span class="text-red-500"></span>
+                            </td>
+                            <td>
+                                <span>Note Here</span>
+                            </td>
+                            <td>
+                                <span>Category Here</span>
+                            </td>
+                        </tr>
+                        <!-- row 2 -->
+                        <tr>
+                            <td>
+                                <span class="font-bold">16 Mei 2023</span>
                             </td>
                             <td>
                                 <span class="text-green-primary"></span>
@@ -86,136 +160,57 @@
                             <td>
                                 <span class="text-red-500">Rp10,000</span>
                             </td>
-                        </tr>
-                        <!-- row 3 -->
-                        <tr>
                             <td>
-                                <div class="flex items-center gap-3">
-                                    <div>
-                                        <span class="font-bold">18 Mei 2023</span>
-                                        <br />
-                                        <span class="text-xs">Note Here</span>
-                                    </div>
-                                </div>
+                                <span>Note Here</span>
                             </td>
                             <td>
-                                <span class="text-green-primary"></span>
-                            </td>
-                            <td>
-                                <span class="text-red-500">Rp10,000</span>
+                                <span>Category Here</span>
                             </td>
                         </tr>
-                        <!-- row 3 -->
+                        <!-- row 1 -->
                         <tr>
                             <td>
-                                <div class="flex items-center gap-3">
-                                    <div>
-                                        <span class="font-bold">18 Mei 2023</span>
-                                        <br />
-                                        <span class="text-xs">Note Here</span>
-                                    </div>
-                                </div>
+                                <span class="font-bold">18 Mei 2023</span>
                             </td>
                             <td>
-                                <span class="text-green-primary"></span>
+                                <span class="text-green-primary">Rp10,000</span>
                             </td>
                             <td>
-                                <span class="text-red-500">Rp10,000</span>
+                                <span class="text-red-500"></span>
+                            </td>
+                            <td>
+                                <span>Note Here</span>
+                            </td>
+                            <td>
+                                <span>Category Here</span>
                             </td>
                         </tr>
-                        <!-- row 3 -->
+                        <!-- row 2 -->
                         <tr>
                             <td>
-                                <div class="flex items-center gap-3">
-                                    <div>
-                                        <span class="font-bold">18 Mei 2023</span>
-                                        <br />
-                                        <span class="text-xs">Note Here</span>
-                                    </div>
-                                </div>
+                                <span class="font-bold">17 Mei 2023</span>
                             </td>
                             <td>
-                                <span class="text-green-primary"></span>
+                                <span class="text-green-primary">Rp10,000</span>
                             </td>
                             <td>
-                                <span class="text-red-500">Rp10,000</span>
-                            </td>
-                        </tr>
-                        <!-- row 3 -->
-                        <tr>
-                            <td>
-                                <div class="flex items-center gap-3">
-                                    <div>
-                                        <span class="font-bold">18 Mei 2023</span>
-                                        <br />
-                                        <span class="text-xs">Note Here</span>
-                                    </div>
-                                </div>
+                                <span class="text-red-500"></span>
                             </td>
                             <td>
-                                <span class="text-green-primary"></span>
+                                <span>Note Here</span>
                             </td>
                             <td>
-                                <span class="text-red-500">Rp10,000</span>
-                            </td>
-                        </tr>
-                        <!-- row 3 -->
-                        <tr>
-                            <td>
-                                <div class="flex items-center gap-3">
-                                    <div>
-                                        <span class="font-bold">18 Mei 2023</span>
-                                        <br />
-                                        <span class="text-xs">Note Here</span>
-                                    </div>
-                                </div>
-                            </td>
-                            <td>
-                                <span class="text-green-primary"></span>
-                            </td>
-                            <td>
-                                <span class="text-red-500">Rp10,000</span>
-                            </td>
-                        </tr>
-                        <!-- row 3 -->
-                        <tr>
-                            <td>
-                                <div class="flex items-center gap-3">
-                                    <div>
-                                        <span class="font-bold">18 Mei 2023</span>
-                                        <br />
-                                        <span class="text-xs">Note Here</span>
-                                    </div>
-                                </div>
-                            </td>
-                            <td>
-                                <span class="text-green-primary"></span>
-                            </td>
-                            <td>
-                                <span class="text-red-500">Rp10,000</span>
-                            </td>
-                        </tr>
-                        <!-- row 3 -->
-                        <tr>
-                            <td>
-                                <div class="flex items-center gap-3">
-                                    <div>
-                                        <span class="font-bold">18 Mei 2023</span>
-                                        <br />
-                                        <span class="text-xs">Note Here</span>
-                                    </div>
-                                </div>
-                            </td>
-                            <td>
-                                <span class="text-green-primary"></span>
-                            </td>
-                            <td>
-                                <span class="text-red-500">Rp10,000</span>
+                                <span>Category Here</span>
                             </td>
                         </tr>
                     </tbody>
                     <!-- foot -->
                 </table>
+                <div class="join">
+                    <button class="join-item btn">«</button>
+                    <button class="join-item btn">Page 1</button>
+                    <button class="join-item btn">»</button>
+                </div>
             </div>
         </div>
     </div>
