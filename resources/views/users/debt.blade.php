f35954ba3629e48427afd538885de7ad0636aebb
takachan0012 <jj4068851@gmail.com>
2024-05-20 17:20:32 +0700

    feat: add search field

--- resources/views/users/debt.blade.php
+++ resources/views/users/debt.blade.php
@@ -37,6 +37,17 @@
                     <span>20,000</span>
                 </div>
             </div>
+            <form action="" class="flex gap-3 my-4">
+                <x-label-input>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="w-4 h-4 opacity-70">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+                    </svg>
+                    <input type="text" class="grow" placeholder="Search Name" name="customer-name" />
+                </x-label-input>
+                <button type="submit" class="btn btn-primary">Search</button>
+            </form>
             <table class="table">
                 <!-- head -->
                 <thead>
