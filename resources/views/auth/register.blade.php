0b702fcd1b6b8b394a85c324c5547f5147e6e9f4
takachan0012 <jj4068851@gmail.com>
2024-05-23 23:27:56 +0700

    feat: email error duplicated

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -20,23 +20,26 @@
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor"
                         class="w-4 h-4 opacity-70">
                         <path
+                            d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
+                    </svg>
+                    <input type="text" class="grow" placeholder="Name" name="name" required />
+                </x-label-input>
+                <x-label-input>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor"
+                        class="w-4 h-4 opacity-70">
+                        <path
                             d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
                         <path
                             d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
                     </svg>
                     <input type="text" class="grow" placeholder="Email" name="email" required />
                 </x-label-input>
-                <x-label-input>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor"
-                        class="w-4 h-4 opacity-70">
-                        <path
-                            d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
-                    </svg>
-                    <input type="text" class="grow" placeholder="Username" name="username" required />
-                </x-label-input>
                 <x-password-input />
                 @error('password')
                     <span class="text-red-500">Password must be more than 8 characters</span>
+                @enderror
+                @error('email')
+                    <span class="text-red-500">Email already exist</span>
                 @enderror
                 <button type="submit" class="btn btn-primary">Register</button>
                 <div>
