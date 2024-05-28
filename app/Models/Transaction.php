601e40796600cf8622802ed9c66a1ff25f732495
takachan0012 <jj4068851@gmail.com>
2024-05-28 15:12:11 +0700

    feat: add uuid

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -3,13 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Transaction extends Model
 {
     use HasFactory;
+    use HasUuids;
 
+
+    protected $primaryKey = 'id';
     protected $fillable = [
         'user_id',
         'status_id',
